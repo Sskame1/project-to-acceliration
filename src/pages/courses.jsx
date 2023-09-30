3fd04a69ea30331cac2a2d834721555de372e1a2
JustAnatolyDev <119749324+Sskame1@users.noreply.github.com>
2023-09-30 19:14:33 +0300

    update

--- src/pages/courses.jsx
+++ src/pages/courses.jsx
@@ -1,7 +1,18 @@
+import { Cards } from "../components/cards"
+
 const Courses = () => {
     return (
         <div id="main">
-
+            <div id="container">
+                <Cards />
+                <Cards />
+                <Cards />
+                <Cards />
+                <Cards />
+                <Cards />
+                <Cards />
+                <Cards />
+            </div>
         </div>
     )
 }
